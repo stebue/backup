4c8947faf00f0327c0905317c259be450c943a92
stefan <stebueh@web.de>
2024-11-10 17:14:12 +0100

    archive backup added

---
+++ backup_archive/backup.bat
@@ -0,0 +1,6 @@
+robocopy F:\archive\bilder                     E:\archive\bilder                    /mir  /log:archive_bilder.log                     /NP 
+robocopy F:\archive\data_linux_eth             E:\archive\data_linux_eth            /mir  /log:archive_data_linux_eth.log             /NP  
+robocopy F:\archive\data_win_acer_stefan       E:\archive\data_win_acer_stefan      /mir  /log:archive_data_win_acer_stefan.log       /NP 
+robocopy F:\archive\data_win_eth               E:\archive\data_win_eth              /mir  /log:archive_data_win_eth.log               /NP 
+robocopy F:\archive\data_win_stefan            E:\archive\data_win_stefan           /mir  /log:archive_data_win_stefan.log            /NP 
+robocopy F:\archive\data_win_tannenweg_stefan  E:\archive\data_win_tannenweg_stefan /mir  /log:archive_data_win_tannenweg_stefan.log  /NP
