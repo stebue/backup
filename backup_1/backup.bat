f85258befe5e8d9182888894e0a37e699800578e
stefan <stebueh@web.de>
2024-11-10 16:57:22 +0100

    initialized

---
+++ backup_1/backup.bat
@@ -0,0 +1,9 @@
+robocopy C:\Users\stebu\data\admin   F:\data\admin    /mir  /log:backup_admin.log    /NP
+
+robocopy C:\Users\stebu\data\stefan  F:\data\stefan   /mir  /log:backup_stefan.log   /NP 
+robocopy C:\Users\stebu\data\Andrea  F:\data\Andrea   /mir  /log:backup_Andrea.log   /NP
+robocopy C:\Users\stebu\data\Leo     F:\data\Leo      /mir  /log:backup_leo.log      /NP
+robocopy C:\Users\stebu\data\Emil    F:\data\Emil     /mir  /log:backup_emil.log     /NP
+robocopy C:\Users\stebu\data\Paula   F:\data\Paula    /mir  /log:backup_paula.log    /NP
+
+robocopy C:\Users\stebu\data\bilder  F:\data\bilder   /mir  /log:backup_bilder.log   /NP
---
+++ backup_2/backup.bat
@@ -0,0 +1,9 @@
+robocopy C:\Users\stebu\data\admin   E:\data\admin    /mir  /log:backup_admin.log    /NP
+
+robocopy C:\Users\stebu\data\stefan  E:\data\stefan   /mir  /log:backup_stefan.log   /NP 
+robocopy C:\Users\stebu\data\Andrea  E:\data\Andrea   /mir  /log:backup_Andrea.log   /NP
+robocopy C:\Users\stebu\data\Leo     E:\data\Leo      /mir  /log:backup_leo.log      /NP
+robocopy C:\Users\stebu\data\Emil    E:\data\Emil     /mir  /log:backup_emil.log     /NP
+robocopy C:\Users\stebu\data\Paula   E:\data\Paula    /mir  /log:backup_paula.log    /NP
+
+robocopy C:\Users\stebu\data\bilder  E:\data\bilder   /mir  /log:backup_bilder.log   /NP
